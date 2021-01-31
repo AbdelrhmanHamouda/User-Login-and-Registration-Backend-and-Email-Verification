35cf8f995aee1b8f781db3c981d2f4c25dad54a4
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-01-31 17:13:59 +0100

    Expand RegistrationService with registration logic

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package com.example.demo.registration;
 
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRole;
+import com.example.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+        return appUserService.signUpUSer(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
