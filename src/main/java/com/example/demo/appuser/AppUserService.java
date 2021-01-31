945d02726ef0fa23f988bcbf1e900a215ca61485
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-01-31 17:56:20 +0100

    Updated default values and added values default constructors

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -1,19 +1,23 @@
 package com.example.demo.appuser;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 
 @Service
-@AllArgsConstructor
+@Data
 public class AppUserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND_MESSAGE = "User with email {0} not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -24,6 +28,20 @@ public class AppUserService implements UserDetailsService {
     }
 
     public String signUpUSer(AppUser appUser) {
-        return "";
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
+                .isPresent();
+        if (userExists) {
+            throw new IllegalStateException("Email already registered");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        //TODO: Send confirmation token
+
+        return "Token sent";
     }
 }
